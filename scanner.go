dca5f3387e6653d7bed96c2be061e7e0f701e2fa
Snehitha <namburisnehitha@gmail.com>
2026-05-19 15:25:21 +0000

    complete scanner

--- scanner.go
+++ scanner.go
@@ -17,3 +17,77 @@ func NewScanner(source string) *Scanner {
 		Tokens:  []Token{},
 	}
 }
+
+func (s *Scanner) Scan() []Token {
+	for s.Current != len(s.Source) {
+		s.Start = s.Current
+		s.scanToken()
+	}
+	s.Tokens = append(s.Tokens, Token{
+		TokenType: EOF,
+	})
+	return s.Tokens
+}
+
+func (s *Scanner) advance() byte {
+	current := s.Source[s.Current]
+	s.Current += 1
+	return current
+}
+
+func (s *Scanner) makeToken(tokenType TokenType) Token {
+	return Token{
+		TokenType: tokenType,
+		Lexeme:    s.Source[s.Start:s.Current],
+		Line:      s.Line,
+		Literal:   nil,
+	}
+}
+
+func (s *Scanner) scanToken() {
+	c := s.advance()
+	switch c {
+	case '(':
+		s.Tokens = append(s.Tokens, s.makeToken(LPAREN))
+	case ')':
+		s.Tokens = append(s.Tokens, s.makeToken(RPAREN))
+	case '{':
+		s.Tokens = append(s.Tokens, s.makeToken(LBRACE))
+	case '}':
+		s.Tokens = append(s.Tokens, s.makeToken(RBRACE))
+	case ',':
+		s.Tokens = append(s.Tokens, s.makeToken(COMMA))
+	case '+':
+		s.Tokens = append(s.Tokens, s.makeToken(PLUS))
+	case ' ':
+		break
+	case '\t':
+		break
+	case '\n':
+		s.Line += 1
+	default:
+		s.Tokens = append(s.Tokens, s.makeToken(ILLEGAL))
+	}
+}
+
+func isAlpha(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+}
+
+func isDigit(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func isAlphaNumeric(c byte) bool {
+	return isAlpha(c) || isDigit(c)
+}
+
+var keyWords = map[string]TokenType{
+	"func":   FUN,
+	"return": RETURN,
+	"int":    INT,
+}
+
+func (s *Scanner) isAtEnd() bool {
+	return s.Current >= len(s.Source)
+}
