0f559d53f0412e6de7e8fa899cef4f11d4a7ea3d
Snehitha <namburisnehitha@gmail.com>
2026-06-02 17:10:31 +0000

    improve test func

--- main.go
+++ main.go
@@ -9,7 +9,22 @@ func main() {
     span := tracer.start(ctx, context)
     return response, nil
 }`
+
 	tokens := NewScanner(source).Scan()
+
 	ast := NewParser(tokens).ParseFunc()
-	fmt.Printf("%+v\n", ast)
+
+	for _, stmt := range ast.Body.List {
+		switch s := stmt.(type) {
+		case *AssignStmt:
+			callExpr := s.Rhs[0].(*CallExpr)
+			fmt.Printf("CallExpr.Fun: %+v\n", callExpr.Fun)
+			fmt.Printf("CallExpr.Args: %+v\n", callExpr.Args)
+			fmt.Printf("AssignStmt: %+v\n", s)
+			fmt.Printf("Rhs[0]: %+v\n", s.Rhs[0])
+		case *ReturnStmt:
+			fmt.Printf("ReturnStmt: %+v\n", s)
+		}
+	}
+
 }
